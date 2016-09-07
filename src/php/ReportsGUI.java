646f0ee5fbf89a46baa785fb3a766c18cb15961f
nwaleiole <junior1830@gmail.com>
2016-09-08 04:53:00 +1000

    updated reports tab

--- src/php/ReportsGUI.java
+++ src/php/ReportsGUI.java
@@ -2,55 +2,89 @@ package php;
 
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
+
+import com.toedter.calendar.JDateChooser;
 
 
 public class ReportsGUI {
-	TextField weeklyTextField = new TextField();
-	TextField monthlyTextField = new TextField();
-		
+	
+	JDateChooser startDate = new JDateChooser();
+	JDateChooser endDate = new JDateChooser();
+	Button printButton = new Button("PRINT");
+	JRadioButton weeklyRadioButton = new JRadioButton("Weekly");
+	JRadioButton monthlyRadioButton = new JRadioButton("Monthly");
+	
+
+	JLabel monthlyLabel = new JLabel("Monthly");
+	JLabel startDateLabel = new JLabel("Start Date");
+	JLabel endDateLabel = new JLabel("End Date");
+	
 	
 	public void ReportsContent(JPanel panel) {
+		
+		
 		GroupLayout layout = new GroupLayout(panel);
 		panel.setLayout(layout);
 		
 		layout.setAutoCreateGaps(true);
 		layout.setAutoCreateContainerGaps(true);
 		
-		Dimension labelSize = new Dimension(50, 22);
+		Dimension buttonSize = new Dimension(50, 30);
+		Dimension dateFieldSize = new Dimension(150, 22);
 		
-		Label weeklyLabel = new Label("Weekly");
-		weeklyLabel.setAlignment(Label.RIGHT);
-		weeklyLabel.setName("weeklyLabel");
-		weeklyLabel.setMaximumSize(labelSize);
+		printButton.setMaximumSize(buttonSize);
+		endDate.setMaximumSize(dateFieldSize);
+		startDate.setMaximumSize(dateFieldSize);
 		
-		Label monthlyLabel = new Label("Monthly");
-		monthlyLabel.setAlignment(Label.RIGHT);
-		monthlyLabel.setName("monthlyLabel");
-		monthlyLabel.setMaximumSize(labelSize);
-		
-		//Dimension buttonSize = new Dimension(50, 30);
-		
-	
 		
 		layout.setHorizontalGroup(
 				layout.createSequentialGroup()
+					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+						.addComponent(weeklyRadioButton)
+						.addComponent(startDateLabel)
+						.addComponent(endDateLabel))
 					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-						.addComponent(weeklyLabel)
-						.addComponent(monthlyLabel)
+						.addComponent(monthlyRadioButton)
+						.addComponent(startDate)
+						.addComponent(endDate)
+						.addComponent(printButton))
+					
 			
-			));
+			);
 			layout.setVerticalGroup(
 				layout.createSequentialGroup()
+					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+						.addComponent(weeklyRadioButton)
+						.addComponent(monthlyRadioButton))
 					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-						.addComponent(weeklyLabel))
+						.addComponent(startDateLabel)
+						.addGroup(layout.createSequentialGroup()
+						.addComponent(startDate)))
 					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-						.addComponent(monthlyLabel))
+						.addComponent(endDateLabel)
+						.addGroup(layout.createSequentialGroup()
+						.addComponent(endDate)
+						.addComponent(printButton)))
 					
 			);
 	}
-
-	
+	//work in progress
+	private void printButtonFunction() {
+		printButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				//this is just to test the print button
+			/*	PrintFunction output = new PrintFunction();
+				output.openFile();
+				output.addRecords();
+				output.closeFile();
+				JOptionPane.showMessageDialog(null, "report printed");	
+			*/
+			}
+		});
+	}
 	
 	
 	
