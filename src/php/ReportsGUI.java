bc7306cbae967c04ba87723132fef4a77def7e8d
MalithiPeiris <malithipeiris66@gmail.com>
2016-09-08 13:51:46 +1000

    JTable works
    Jtable works

--- src/php/ReportsGUI.java
+++ src/php/ReportsGUI.java
@@ -1,5 +1,20 @@
 package php;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import javax.swing.JComponent;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.List;
@@ -40,7 +55,7 @@ public class ReportsGUI {
 		SearchButtonFunction();
 
 		reportTable.setModel(new DefaultTableModel(new Object[][] { { null, null, null } },
-				new String[] { "Name", "Quentity", "Added Date" }));
+				new String[] { "SALEID", "PRODUCT_ID", "SALE_DATE" , "QTYSOLD" }));
 		reportTableScoller.setViewportView(reportTable);
 
 		layout.setHorizontalGroup(layout.createSequentialGroup()
@@ -72,8 +87,46 @@ public class ReportsGUI {
 	                model.setRowCount(0);
 	                reportTable.setModel(GetModelToTable(reportTable, DatabaseIO.reportSaleList));
 	                
+	                
+	                class MyComponent extends JComponent {
+	                	   Slice[] slices = { new Slice(5, Color.black), 
+	                	   new Slice(33, Color.green),
+	                	   new Slice(20, Color.yellow), new Slice(15, Color.red) };
+	                	   MyComponent() {}
+	                	   public void paint(Graphics g) {
+	                	      drawPie((Graphics2D) g, getBounds(), slices);
+	                	   }
+	                	   void drawPie(Graphics2D g, Rectangle area, Slice[] slices) {
+	                	      double total = 0.0D;
+	                	      for (int i = 0; i < slices.length; i++) {
+	                	         total += slices[i].value;
+	                	      }
+	                	      double curValue = 0.0D;
+	                	      int startAngle = 0;
+	                	      for (int i = 0; i < slices.length; i++) {
+	                	         startAngle = (int) (curValue * 360 / total);
+	                	         int arcAngle = (int) (slices[i].value * 360 / total);
+	                	         g.setColor(slices[i].color);
+	                	         g.fillArc(area.x, area.y, area.width, area.height, 
+	                	         startAngle, arcAngle);
+	                	         curValue += slices[i].value;
+	                	      }
+	                	   }
+	                	   
+	                	}
+	                class Slice {
+	                	   double value;
+	                	   Color color;
+	                	   public Slice(double value, Color color) {  
+	                	      this.value = value;
+	                	      this.color = color;
+	                	   }
+	                	}
+	                
+	                
+	                
 	            }
-	            
+	     
 	        });
 	    }
 
